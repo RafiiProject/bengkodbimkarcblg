c10e98c7cb986871838aea0484bdd2c5fcadc97b
RafiiProject <mzrafi1708@gmail.com>
2025-01-03 23:35:05 +0700

    updates delete pasien

--- page/pasien/hapusPasien.php
+++ page/pasien/hapusPasien.php
@@ -5,11 +5,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Ambil nilai dari form
     $id = $_POST["id"];
 
-    // Query untuk melakukan update data obat
+    // Nonaktifkan foreign key checks
+    mysqli_query($mysqli, "SET FOREIGN_KEY_CHECKS = 0");
+
+    // Query untuk menghapus seluruh pasien
     $query = "DELETE FROM pasien WHERE id = $id";
 
     // Eksekusi query
     if (mysqli_query($mysqli, $query)) {
+        // Aktifkan kembali foreign key checks
+        mysqli_query($mysqli, "SET FOREIGN_KEY_CHECKS = 1");
+
         // Jika berhasil, redirect kembali ke halaman index atau sesuaikan dengan kebutuhan Anda
         echo '<script>';
         echo 'alert("Data pasien berhasil dihapus!");';
@@ -20,6 +26,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Jika terjadi kesalahan, tampilkan pesan error
         echo "Error: " . $query . "<br>" . mysqli_error($mysqli);
     }
+
+    // Aktifkan kembali foreign key checks jika gagal
+    mysqli_query($mysqli, "SET FOREIGN_KEY_CHECKS = 1");
 }
 
 // Tutup koneksi
